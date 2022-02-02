d19854730f44860561049877c3f0d6ada3b823de
grdddj <jiri.musil06@seznam.cz>
2022-02-02 15:24:41 +0100

    nonstandard_path sign_tx

--- tests/device_tests/bitcoin/test_nonstandard_paths.py
+++ tests/device_tests/bitcoin/test_nonstandard_paths.py
@@ -22,56 +22,73 @@ from trezorlib.tools import parse_path
 from ...tx_cache import TxCache
 
 TX_CACHE_MAINNET = TxCache("Bitcoin")
+TX_CACHE_TESTNET = TxCache("Testnet")
 
 TXHASH_6189e3 = bytes.fromhex(
     "6189e3febb5a21cee8b725aa1ef04ffce7e609448446d3a8d6f483c634ef5315"
 )
-TXHASH_d5f65e = bytes.fromhex(
-    "d5f65ee80147b4bcc70b75e4bbf2d7382021b871bd8867ef8fa525ef50864882"
+TXHASH_564f5b = bytes.fromhex(
+    "564f5b97aecb5608d6bdd021856ad55aeecaebd2d61b598b7161004f9619d662"
 )
 
 
-VECTORS = (
+# Index corresponds to UTXO index in a transaction
+VECTORS = (  # path, script_types_and_indexes
     # GreenAddress A m/[1,4]/address_index
     (
         "m/4/255",
         (
-            messages.InputScriptType.SPENDADDRESS,
-            messages.InputScriptType.SPENDWITNESS,
-            messages.InputScriptType.SPENDP2SHWITNESS,
+            # mtTCmwhifioeTxufJ3WY1wvqjfia2NQT8F
+            (messages.InputScriptType.SPENDADDRESS, 7),
+            # tb1q3hjq7v0jlxz0g2v4nntp67ufg65059z45kg8j6
+            (messages.InputScriptType.SPENDWITNESS, 3),
+            # 2N26epREypYGMKf8X325nzBozi5gvjYPwab
+            (messages.InputScriptType.SPENDP2SHWITNESS, 10),
         ),
     ),
     # GreenAddress B m/3'/[1-100]'/[1,4]/address_index
     (
         "m/3'/100'/4/255",
         (
-            messages.InputScriptType.SPENDADDRESS,
-            messages.InputScriptType.SPENDWITNESS,
-            messages.InputScriptType.SPENDP2SHWITNESS,
+            # mtCrtJvkMRYmXpy9vxPxiGskLAmnku4xFb
+            (messages.InputScriptType.SPENDADDRESS, 6),
+            # tb1q3vk6rcwav6f4tadutzgjvvfut4v4fr5uj03v8q
+            (messages.InputScriptType.SPENDWITNESS, 2),
+            # 2NDQqoJ9n7xN7WxmKY36GDd1N7HhcvCmK8y
+            (messages.InputScriptType.SPENDP2SHWITNESS, 11),
         ),
     ),
     # GreenAdress Sign A m/1195487518
     (
         "m/1195487518",
         (
-            messages.InputScriptType.SPENDADDRESS,
-            messages.InputScriptType.SPENDWITNESS,
-            messages.InputScriptType.SPENDP2SHWITNESS,
+            # mnN9Pyq16W2MFif5C26pctxHV7aY9Ke5at
+            (messages.InputScriptType.SPENDADDRESS, 4),
+            # tb1qfv0x0zcd59lzlgdrq04n77pq3pqdc04m3zl7vk
+            (messages.InputScriptType.SPENDWITNESS, 0),
+            # 2MuaBfmxqnoVzubA1kFqedhZg9HBtJFrTJ6
+            (messages.InputScriptType.SPENDP2SHWITNESS, 9),
         ),
     ),
     # GreenAdress Sign B m/1195487518/6/address_index
     (
         "m/1195487518/6/255",
         (
-            messages.InputScriptType.SPENDADDRESS,
-            messages.InputScriptType.SPENDWITNESS,
-            messages.InputScriptType.SPENDP2SHWITNESS,
+            # mnc2ekbsFQbJvpgszDu3kXj9duzLLiKddm
+            (messages.InputScriptType.SPENDADDRESS, 5),
+            # tb1qfklf3clkeve84h50np0nkhrhac9zlyp3m55kj8
+            (messages.InputScriptType.SPENDWITNESS, 1),
+            # 2MuFjiqJ7mtn3Vr9Mq4NfuTeZsKYd1PUByV
+            (messages.InputScriptType.SPENDP2SHWITNESS, 8),
         ),
     ),
     # Casa m/49/coin_type/account/change/address_index
     (
         "m/49/0/63/0/255",
-        (messages.InputScriptType.SPENDP2SHWITNESS,),
+        (
+            # 2NG4J8UkePVX9LbGBX3u6ffYPuLhKD9oCDc
+            (messages.InputScriptType.SPENDP2SHWITNESS, 12),
+        ),
     ),
 )
 
@@ -99,9 +116,9 @@ VECTORS_MULTISIG = (
 
 # Has AlwaysMatchingSchema but let's make sure the nonstandard paths are
 # accepted in case we make this more restrictive in the future.
-@pytest.mark.parametrize("path, script_types", VECTORS)
-def test_getpublicnode(client, path, script_types):
-    for script_type in script_types:
+@pytest.mark.parametrize("path, script_types_and_indexes", VECTORS)
+def test_getpublicnode(client, path, script_types_and_indexes):
+    for script_type, _ in script_types_and_indexes:
         res = btc.get_public_node(
             client, parse_path(path), coin_name="Bitcoin", script_type=script_type
         )
@@ -109,9 +126,9 @@ def test_getpublicnode(client, path, script_types):
         assert res.xpub
 
 
-@pytest.mark.parametrize("path, script_types", VECTORS)
-def test_getaddress(client, path, script_types):
-    for script_type in script_types:
+@pytest.mark.parametrize("path, script_types_and_indexes", VECTORS)
+def test_getaddress(client, path, script_types_and_indexes):
+    for script_type, _ in script_types_and_indexes:
         res = btc.get_address(
             client,
             "Bitcoin",
@@ -123,9 +140,9 @@ def test_getaddress(client, path, script_types):
         assert res
 
 
-@pytest.mark.parametrize("path, script_types", VECTORS)
-def test_signmessage(client, path, script_types):
-    for script_type in script_types:
+@pytest.mark.parametrize("path, script_types_and_indexes", VECTORS)
+def test_signmessage(client, path, script_types_and_indexes):
+    for script_type, _ in script_types_and_indexes:
         sig = btc.sign_message(
             client,
             coin_name="Bitcoin",
@@ -137,28 +154,28 @@ def test_signmessage(client, path, script_types):
         assert sig.signature
 
 
-@pytest.mark.parametrize("path, script_types", VECTORS)
-def test_signtx(client, path, script_types):
+@pytest.mark.parametrize("path, script_types_and_indexes", VECTORS)
+def test_signtx(client, path, script_types_and_indexes):
     # tx: d5f65ee80147b4bcc70b75e4bbf2d7382021b871bd8867ef8fa525ef50864882
     # input 0: 0.0039 BTC
 
-    for script_type in script_types:
+    for script_type, utxo_index in script_types_and_indexes:
         inp1 = messages.TxInputType(
             address_n=parse_path(path),
-            amount=390000,
-            prev_hash=TXHASH_d5f65e,
-            prev_index=0,
+            amount=10_000,
+            prev_hash=TXHASH_564f5b,
+            prev_index=utxo_index,
             script_type=script_type,
         )
 
         out1 = messages.TxOutputType(
             address="1MJ2tj2ThBE62zXbBYA5ZaN3fdve5CPAz1",
-            amount=390000 - 10000,
+            amount=10_000 - 1_000,
             script_type=messages.OutputScriptType.PAYTOADDRESS,
         )
 
         _, serialized_tx = btc.sign_tx(
-            client, "Bitcoin", [inp1], [out1], prev_txes=TX_CACHE_MAINNET
+            client, "Bitcoin", [inp1], [out1], prev_txes=TX_CACHE_TESTNET
         )
 
         assert serialized_tx.hex()
