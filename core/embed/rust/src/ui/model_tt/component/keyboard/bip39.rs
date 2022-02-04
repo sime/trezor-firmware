8f0e26830696913f3312c3cd3c3d102f3daf62f7
Jan Pochyla <jpochyla@gmail.com>
2022-02-04 14:21:28 -0300

    chore(core/rust): Use Maybe wrapper in BIP39 KB

--- core/embed/rust/src/ui/model_tt/component/keyboard/bip39.rs
+++ core/embed/rust/src/ui/model_tt/component/keyboard/bip39.rs
@@ -1,7 +1,7 @@
 use crate::{
     trezorhal::bip39,
     ui::{
-        component::{Child, Component, Event, EventCtx},
+        component::{Child, Component, Event, EventCtx, Label, Maybe},
         display,
         geometry::{Grid, Offset, Rect},
         model_tt::{
@@ -25,6 +25,200 @@ const KEYS: [&[u8]; 9] = [
 
 const MAX_WORD_LEN: usize = 8;
 
+pub struct Bip39Keyboard {
+    /// Initial prompt, displayed on empty input.
+    prompt: Child<Maybe<Label<&'static [u8]>>>,
+    /// Backspace button.
+    back: Child<Maybe<Button>>,
+    /// Input area, acting as the auto-complete and confirm button.
+    input: Child<Maybe<Bip39Input>>,
+    /// Key buttons.
+    keys: [Child<Button>; KEYS.len()],
+}
+
+impl Bip39Keyboard {
+    pub fn new(area: Rect, prompt: &'static [u8]) -> Self {
+        let grid = Grid::new(area, 3, 4);
+        let back_area = grid.row_col(0, 0);
+        let input_area = grid.row_col(0, 1).union(grid.row_col(0, 3));
+        let prompt_area = grid.row_col(0, 0).union(grid.row_col(0, 3));
+        let prompt_origin = prompt_area.top_left();
+
+        Self {
+            prompt: Child::new(Maybe::visible(
+                prompt_area,
+                theme::BG,
+                Label::left_aligned(prompt_origin, prompt, theme::label_default()),
+            )),
+            back: Child::new(Maybe::hidden(
+                back_area,
+                theme::BG,
+                Button::with_icon(back_area, theme::ICON_BACK).styled(theme::button_clear()),
+            )),
+            input: Child::new(Maybe::hidden(
+                input_area,
+                theme::BG,
+                Bip39Input::new(input_area),
+            )),
+            keys: Self::key_buttons(&grid, grid.cols), // Start in the second row.
+        }
+    }
+
+    fn key_buttons(grid: &Grid, offset: usize) -> [Child<Button>; KEYS.len()] {
+        array_map_enumerate(KEYS, |index, text| {
+            Child::new(Button::with_text(grid.cell(offset + index), text))
+        })
+    }
+
+    /// Either enable or disable the key buttons, depending on the dictionary
+    /// completion mask and the pending key.
+    fn toggle_key_buttons(&mut self, ctx: &mut EventCtx) {
+        let pending_key = self.input.inner().inner().multi_tap.pending_key();
+        let mask = self.input.inner().inner().completion_mask();
+
+        for (key, btn) in self.keys.iter_mut().enumerate() {
+            // Currently pending key is always enabled.
+            let key_is_pending = pending_key == Some(key);
+            // Keys that contain letters from the completion mask are enabled as well.
+            let key_matches_mask = Self::compute_mask(KEYS[key]) & mask != 0;
+            btn.mutate(ctx, |ctx, b| {
+                b.enable_if(ctx, key_is_pending || key_matches_mask)
+            });
+        }
+    }
+
+    /// After edit operations, we need to either show or hide the prompt, the
+    /// input, and the back button.
+    fn toggle_prompt_or_input(&mut self, ctx: &mut EventCtx) {
+        let prompt_visible = self.input.inner().inner().textbox.is_empty();
+        self.prompt
+            .mutate(ctx, |ctx, p| p.show_if(ctx, prompt_visible));
+        self.input
+            .mutate(ctx, |ctx, i| i.show_if(ctx, !prompt_visible));
+        self.back
+            .mutate(ctx, |ctx, b| b.show_if(ctx, !prompt_visible));
+    }
+
+    /// Compute a bitmask of all letters contained in given key text. Lowest bit
+    /// is 'a', second lowest 'b', etc.
+    fn compute_mask(key_text: &[u8]) -> u32 {
+        let mut mask = 0;
+        for ch in key_text {
+            // We assume the key text is lower-case alphabetic ASCII, making the subtraction
+            // and the shift panic-free.
+            mask |= 1 << (ch - b'a');
+        }
+        mask
+    }
+
+    /// Key button was clicked. If this button is pending, let's cycle the
+    /// pending character in textbox. If not, let's just append the first
+    /// character.
+    fn on_key_click(&mut self, ctx: &mut EventCtx, clicked_key: usize) {
+        self.input.mutate(ctx, |ctx, input| {
+            let input = input.inner_mut();
+            input
+                .multi_tap
+                .click_key(ctx, clicked_key, KEYS[clicked_key], &mut input.textbox);
+            input.complete_word_from_dictionary();
+        });
+        self.toggle_key_buttons(ctx);
+        self.toggle_prompt_or_input(ctx);
+    }
+
+    /// Backspace button was clicked, let's delete the last character of input
+    /// and clear the pending marker.
+    fn on_backspace_click(&mut self, ctx: &mut EventCtx) {
+        self.input.mutate(ctx, |ctx, input| {
+            let input = input.inner_mut();
+            input.textbox.delete_last(ctx);
+            input.multi_tap.clear_pending_state(ctx);
+        });
+        self.toggle_key_buttons(ctx);
+        self.toggle_prompt_or_input(ctx);
+    }
+
+    /// Input button was clicked.  If the content matches the suggested word,
+    /// let's confirm it, otherwise just auto-complete.
+    fn on_input_click(&mut self, ctx: &mut EventCtx) -> Option<Bip39KeyboardMsg> {
+        let msg = self.input.mutate(ctx, |ctx, input| {
+            let input = input.inner_mut();
+            match input.completed_word {
+                Some(word) if word == input.textbox.content() => {
+                    input.textbox.clear(ctx);
+                    Some(Bip39KeyboardMsg::Confirmed)
+                }
+                Some(word) => {
+                    input.textbox.replace(ctx, word);
+                    None
+                }
+                None => None,
+            }
+        });
+        self.toggle_key_buttons(ctx);
+        self.toggle_prompt_or_input(ctx);
+        msg
+    }
+
+    /// Timeout occurred.  If we can auto-complete current input, let's just
+    /// reset the pending marker.  If not, input is invalid, let's backspace the
+    /// last character.
+    fn on_timeout(&mut self, ctx: &mut EventCtx) {
+        self.input.mutate(ctx, |ctx, input| {
+            let input = input.inner_mut();
+            if input.completed_word.is_none() {
+                input.textbox.delete_last(ctx);
+            }
+            input.multi_tap.clear_pending_state(ctx);
+        });
+        self.toggle_key_buttons(ctx);
+        self.toggle_prompt_or_input(ctx);
+    }
+}
+
+impl Component for Bip39Keyboard {
+    type Msg = Bip39KeyboardMsg;
+
+    fn event(&mut self, ctx: &mut EventCtx, event: Event) -> Option<Self::Msg> {
+        match self.input.event(ctx, event) {
+            Some(Bip39InputMsg::Button(ButtonMsg::Clicked)) => {
+                if let Some(msg) = self.on_input_click(ctx) {
+                    return Some(msg);
+                }
+            }
+            Some(Bip39InputMsg::TimedOut) => {
+                self.on_timeout(ctx);
+            }
+            _ => {}
+        }
+        if let Some(ButtonMsg::Clicked) = self.back.event(ctx, event) {
+            self.on_backspace_click(ctx);
+            return None;
+        }
+        for (key, btn) in self.keys.iter_mut().enumerate() {
+            if let Some(ButtonMsg::Clicked) = btn.event(ctx, event) {
+                self.on_key_click(ctx, key);
+                return None;
+            }
+        }
+        None
+    }
+
+    fn paint(&mut self) {
+        self.prompt.paint();
+        self.input.paint();
+        self.back.paint();
+        for btn in &mut self.keys {
+            btn.paint();
+        }
+    }
+}
+
+enum Bip39InputMsg {
+    Button(ButtonMsg),
+    TimedOut,
+}
+
 struct Bip39Input {
     button: Button,
     textbox: TextBox<MAX_WORD_LEN>,
@@ -35,7 +229,7 @@ struct Bip39Input {
 impl Bip39Input {
     fn new(area: Rect) -> Self {
         Self {
-            button: Button::empty(area, theme::button_default()),
+            button: Button::empty(area),
             textbox: TextBox::empty(),
             multi_tap: MultiTapKeyboard::new(),
             completed_word: None,
@@ -52,10 +246,13 @@ impl Bip39Input {
 }
 
 impl Component for Bip39Input {
-    type Msg = ButtonMsg;
+    type Msg = Bip39InputMsg;
 
     fn event(&mut self, ctx: &mut EventCtx, event: Event) -> Option<Self::Msg> {
-        self.button.event(ctx, event)
+        match (event, self.multi_tap.pending_timer()) {
+            (Event::Timer(t), Some(pt)) if pt == t => Some(Bip39InputMsg::TimedOut),
+            _ => self.button.event(ctx, event).map(Bip39InputMsg::Button),
+        }
     }
 
     fn paint(&mut self) {
@@ -111,160 +308,6 @@ impl Component for Bip39Input {
             // 16px from the right edge.
             let icon_center = area.top_right().center(area.bottom_right()) - Offset::new(16 + 8, 0);
             display::icon(icon_center, icon, style.text_color, style.button_color);
-        }
-    }
-}
-
-pub struct Bip39Keyboard {
-    input: Child<Bip39Input>,
-    back: Child<Button>,
-    keys: [Child<Button>; KEYS.len()],
-}
-
-impl Bip39Keyboard {
-    pub fn new(area: Rect) -> Self {
-        let grid = Grid::new(area, 3, 4);
-        let back_area = grid.row_col(0, 0);
-        let input_area = grid.row_col(0, 1).union(grid.row_col(0, 2));
-        Self {
-            input: Child::new(Bip39Input::new(input_area)),
-            back: Child::new(Button::with_icon(
-                back_area,
-                theme::ICON_BACK,
-                theme::button_clear(),
-            )),
-            keys: Self::key_buttons(&grid, grid.cols), // Start in the second row.
-        }
-    }
-
-    fn key_buttons(grid: &Grid, offset: usize) -> [Child<Button>; KEYS.len()] {
-        array_map_enumerate(KEYS, |index, text| {
-            Child::new(Button::with_text(
-                grid.cell(offset + index),
-                text,
-                theme::button_default(),
-            ))
-        })
-    }
-
-    /// Either enable or disable the key buttons, depending on the dictionary
-    /// completion mask and the pending key.
-    fn toggle_key_buttons(&mut self, ctx: &mut EventCtx) {
-        let pending_key = self.input.inner().multi_tap.pending_key();
-        let mask = self.input.inner().completion_mask();
-
-        for (key, btn) in self.keys.iter_mut().enumerate() {
-            // Currently pending key is always enabled.
-            let key_is_pending = pending_key == Some(key);
-            // Keys that contain letters from the completion mask are enabled as well.
-            let key_matches_mask = Self::compute_mask(KEYS[key]) & mask != 0;
-            btn.mutate(ctx, |ctx, b| {
-                b.enable_if(ctx, key_is_pending || key_matches_mask)
-            });
-        }
-    }
-
-    /// Compute a bitmask of all letters contained in given key text. Lowest bit
-    /// is 'a', second lowest 'b', etc.
-    fn compute_mask(key_text: &[u8]) -> u32 {
-        let mut mask = 0;
-        for ch in key_text {
-            // We assume the key text is lower-case alphabetic ASCII, making the subtraction
-            // and the shift panic-free.
-            mask |= 1 << (ch - b'a');
-        }
-        mask
-    }
-
-    /// Key button was clicked. If this button is pending, let's cycle the
-    /// pending character in textbox. If not, let's just append the first
-    /// character.
-    fn on_key_click(&mut self, ctx: &mut EventCtx, clicked_key: usize) {
-        self.input.mutate(ctx, |ctx, input| {
-            input
-                .multi_tap
-                .click_key(ctx, clicked_key, KEYS[clicked_key], &mut input.textbox);
-            input.complete_word_from_dictionary();
-        });
-        self.toggle_key_buttons(ctx);
-    }
-
-    /// Backspace button was clicked, let's delete the last character of input
-    /// and clear the pending marker.
-    fn on_backspace_click(&mut self, ctx: &mut EventCtx) {
-        self.input.mutate(ctx, |ctx, input| {
-            input.textbox.delete_last(ctx);
-            input.multi_tap.clear_pending_state(ctx);
-        });
-        self.toggle_key_buttons(ctx);
-    }
-
-    /// Input button was clicked.  If the content matches the suggested word,
-    /// let's confirm it, otherwise just auto-complete.
-    fn on_input_click(&mut self, ctx: &mut EventCtx) -> Option<Bip39KeyboardMsg> {
-        let msg = self
-            .input
-            .mutate(ctx, |ctx, input| match input.completed_word {
-                Some(word) if word == input.textbox.content() => {
-                    input.textbox.clear(ctx);
-                    Some(Bip39KeyboardMsg::Confirmed)
-                }
-                Some(word) => {
-                    input.textbox.replace(ctx, word);
-                    None
-                }
-                None => None,
-            });
-        self.toggle_key_buttons(ctx);
-        msg
-    }
-
-    /// Timeout occurred.  If we can auto-complete current input, let's just
-    /// reset the pending marker.  If not, input is invalid, let's backspace the
-    /// last character.
-    fn on_timeout(&mut self, ctx: &mut EventCtx) {
-        self.input.mutate(ctx, |ctx, input| {
-            if input.completed_word.is_none() {
-                input.textbox.delete_last(ctx);
-            }
-            input.multi_tap.clear_pending_state(ctx);
-        });
-        self.toggle_key_buttons(ctx);
-    }
-}
-
-impl Component for Bip39Keyboard {
-    type Msg = Bip39KeyboardMsg;
-
-    fn event(&mut self, ctx: &mut EventCtx, event: Event) -> Option<Self::Msg> {
-        let pending_timer = self.input.inner().multi_tap.pending_timer();
-        if matches!((event, pending_timer), (Event::Timer(t), Some(pt)) if pt == t) {
-            self.on_timeout(ctx);
-            return None;
-        }
-        if let Some(ButtonMsg::Clicked) = self.input.event(ctx, event) {
-            if let Some(msg) = self.on_input_click(ctx) {
-                return Some(msg);
-            }
-        }
-        if let Some(ButtonMsg::Clicked) = self.back.event(ctx, event) {
-            self.on_backspace_click(ctx);
-            return None;
-        }
-        for (key, btn) in self.keys.iter_mut().enumerate() {
-            if let Some(ButtonMsg::Clicked) = btn.event(ctx, event) {
-                self.on_key_click(ctx, key);
-                return None;
-            }
-        }
-        None
-    }
-
-    fn paint(&mut self) {
-        self.input.paint();
-        self.back.paint();
-        for btn in &mut self.keys {
-            btn.paint();
         }
     }
 }
