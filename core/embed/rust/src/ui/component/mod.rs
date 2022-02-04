b3b56d28a2d36688ec04d87153301e137473978a
Jan Pochyla <jpochyla@gmail.com>
2022-02-04 14:21:09 -0300

    chore(core/rust): Add WIP Maybe wrapper

--- core/embed/rust/src/ui/component/mod.rs
+++ core/embed/rust/src/ui/component/mod.rs
@@ -4,6 +4,7 @@ pub mod base;
 pub mod empty;
 pub mod label;
 pub mod map;
+pub mod maybe;
 pub mod pad;
 pub mod text;
 pub mod tuple;
@@ -11,6 +12,7 @@ pub mod tuple;
 pub use base::{Child, Component, ComponentExt, Event, EventCtx, Never, TimerToken};
 pub use empty::Empty;
 pub use label::{Label, LabelStyle};
+pub use maybe::Maybe;
 pub use pad::Pad;
 pub use text::{
     formatted::FormattedText,
