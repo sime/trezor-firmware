de700fc022fb3e3d28e349c4acedbf7ea0215427
grdddj <jiri.musil06@seznam.cz>
2022-02-09 14:21:18 +0100

    fixup! fix(ci): add T1/TT argument into prepare_ui_artifacts.py to account for missing files

--- ci/prepare_ui_artifacts.py
+++ ci/prepare_ui_artifacts.py
@@ -13,15 +13,14 @@ from tests.ui_tests import _hash_files, FILE_HASHES, SCREENS_DIR  # isort:skip
 # As in CI we are running T1 and TT tests separately, there will
 # always be the other model missing.
 # Therefore, choosing just the cases for our model.
-if len(sys.argv) > 1 and sys.argv[1].lower() == "t1":
+if len(sys.argv) > 1 and sys.argv[1].upper() == "T1":
     model = "T1"
 else:
     model = "TT"
 model_file_hashes = {k: v for k, v in FILE_HASHES.items() if k.startswith(f"{model}_")}
 
-for test_case in model_file_hashes.keys():
+for test_case, expected_hash in model_file_hashes.items():
     recorded_dir = SCREENS_DIR / test_case / "recorded"
-    expected_hash = model_file_hashes[test_case]
     actual_hash = _hash_files(recorded_dir)
     assert expected_hash == actual_hash
     shutil.make_archive(
